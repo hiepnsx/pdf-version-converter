9c31586a776d5204af58615a4599bb64261e2ec1
hiepnsx <hiep@verve-inc.jp>
2020-05-22 13:19:37 +0900

    Remove escapeshellarg

--- src/Converter/GhostscriptConverterCommand.php
+++ src/Converter/GhostscriptConverterCommand.php
@@ -30,7 +30,7 @@ class GhostscriptConverterCommand
 
     public function run($originalFile, $newFile, $newVersion)
     {
-        $command = sprintf($this->baseCommand, $newVersion, $newFile, escapeshellarg($originalFile));
+        $command = sprintf($this->baseCommand, $newVersion, $newFile, $originalFile);
 
         $process = new Process($command);
         $process->run();
